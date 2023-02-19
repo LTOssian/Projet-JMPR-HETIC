5f95633ba64cc11e2ebe2e850de6da006e990bfc
AlessGarau <alessandrog.pro@gmail.com>
2023-02-19 11:15:54 +0100

    feat: add credit page

--- views/creditsComp.js
+++ views/creditsComp.js
@@ -2,25 +2,69 @@ import {home} from './homeComp.js'
 
 class Credits {
     constructor() {
-        this.html = ``;
-        this.css = ``;
+        this.html = `
+        <main>
+            <div class="home-logo"></div>
+            <h1>CREDITS</h1>
+            <div class="window-container">
+                <div class="card runner">
+                    <h2 class="card-name">Runner</h2>
+                    <div class="separate"></div>
+                    <ul>
+                        <li class="first-li">Benjamin SCHINKEL</li>
+                        <li>Julien HEITZ</li>
+                        <li>Alexis JANUS</li>
+                        <li>Victor CHABEAU</li>
+                    </ul>
+                </div>
+                <div id="vertical"></div>
+                <div class="card editor">
+                    <h2 class="card-name">Editor</h2>
+                    <div class="separate"></div>
+                    <ul>
+                        <li class="first-li">Alessandro GARAU</li>
+                        <li>Djedje GBOBLE</li>
+                        <li>Louisan TCHITOULA </li>
+                        <li>Mohamed SALAMATAO</li>
+                    </ul>
+                </div>
+            </div>
+        </main>
+        `;
+        this.css = `
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+            <link rel="stylesheet" href="./styles/credits.css">
+        `;
     }
 
-    updateCSS() {
-
+    updateCSS () {
+        document.querySelector('head').innerHTML = this.css;
     }
 
-    updateHTML() {
-
+    updateHTML () {
+        document.querySelector('body').innerHTML = this.html;
     }
 
-    mount() {
-
+    mount () {
+        this.updateCSS();
+        this.updateHTML();
     }
-    
-    unmount() {
 
+    unmount () {
+        document.querySelector('head').innerHTML = "";
+        document.querySelector('body').innerHTML = "";
     }
+
+    homeRoute(){
+        let goHome = document.querySelector('#creditToHomePage')
+         goHome.addEventListener('click',()=>{
+             this.unmount()
+             home.mount()
+         })
+     }
 }
 
 export const credits = new Credits ();
